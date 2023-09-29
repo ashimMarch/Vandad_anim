f638d61bff593fff94c348b3439831dc4a1ecbfb
ashimMarch <104412075+ashimMarch@users.noreply.github.com>
2023-09-29 11:20:06 +0530

    3d cubic rotaion completed. single animation with multiple controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 
 import 'package:flutter/material.dart';
+
 // import 'package:animation_tutorial/01_animatedbuilder/example1.dart';
-import '02_chained_animation/clippers.dart';
+// import '02_chained_animation/clippers.dart';
+import '03_stack_rotate_3d/widget_rotaion3d.dart';
 
 
 void main() {
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const FlippingHalfCircle(),
+      home: const WidgetRotation3D(),
     );
   }
 }
